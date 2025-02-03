369cb0674a275116fe529e20d25f5a3aa0ec60a0
Oscar Lloyd-John [sc22olj] <sc22olj@leeds.ac.uk>
2025-02-03 11:32:14 +0000

    added stubs for preprocess, import issue

---
+++ preprocess.py
@@ -0,0 +1,43 @@
+from fslpy import *
+from multiprocessing import Pool
+
+
+print(fsl.data.dicom.MIN_DCM2NIIX_VERSION)
+
+def main():
+
+    pass
+
+# Loads a dicom MRI and converts it to nifti
+def dcm_to_nii():
+
+    #fsl.data.dicom
+    #NB this is just a wrapper around dcm2niix
+
+    pass
+
+def mri_to_tensor():
+
+    pass
+
+def brain_extraction():
+
+    pass
+
+# Align all MRIs to the same anatomical space
+def affine_registration():
+
+    #fsl.transform.affine
+
+    pass
+
+# Remove excess space around the image
+def crop():
+
+    pass
+
+
+def intensity_normalisation():
+
+    pass
+
