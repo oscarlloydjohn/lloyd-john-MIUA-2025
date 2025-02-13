89a20e1321a12d9448a2842418cd49c7267e5498
Oscar Lloyd-John [sc22olj] <sc22olj@leeds.ac.uk>
2025-02-13 18:02:46 +0000

    Cnn file now prints multiple images, need to get freesurfer working to view them

--- preprocessing/run_processing.sh
+++ preprocessing/run_processing.sh
@@ -5,10 +5,10 @@ SCRIPT_PATH="/uolstore/home/student_lnxhome01/sc22olj/Compsci/year3/individual-p
 LICENSE_PATH="/vol/scratch/SoC/misc/2024/sc22olj/tools/freesurfer-license/license.txt"
 CONTAINER_PATH="/vol/scratch/SoC/misc/2024/sc22olj/fastsurfer-gpu.sif"
 
-nohup singularity exec --nv \
+singularity exec --nv \
     --no-home \
     -B $DATA_PATH:$DATA_PATH \
     -B $LICENSE_PATH:$LICENSE_PATH \
+    -B $SCRIPT_PATH:$SCRIPT_PATH \
     $CONTAINER_PATH \
-    python3 $SCRIPT_PATH --data_path $DATA_PATH --license_path $LICENSE_PATH --container_path $CONTAINER_PATH > $DATA_PATH/output.log 2>&1 & disown
-
+    python3 $SCRIPT_PATH --data_path $DATA_PATH --license_path $LICENSE_PATH --container_path $CONTAINER_PATH
