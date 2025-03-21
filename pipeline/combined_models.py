88d472ab064e1baefeb92180b91b028d4b2c3741
Oscar Lloyd-John [sc22olj] <sc22olj@leeds.ac.uk>
2025-03-21 18:56:45 +0000

    Need to determine architecture of pipeline and method of loading in scores

--- pipeline/combined_models.py
+++ pipeline/combined_models.py
@@ -22,11 +22,71 @@ from ozzy_torch_utils.init_dataloaders import *
 
 from explain_pointnet import get_prediction
 
-# Load pytorch model
+# Black box wrapper for pointnet, allowing it to take a single input with no batch dimension
+class PointNetWrapper(torch.nn.Module):
+
+    def __init__(self, model):
+        
+        super(PointNetWrapper, self).__init__()
+
+        self.model = model
+
+    def forward(self, x):
+        
+        # Add batch dim and transpose to 3 x n for pointnet
+        x = x.unsqueeze(0).transpose(2, 1)
+
+        # Predict, taking only output and not l3_layer from pointnet
+        x = self.model(x)[0]
+
+        return x
+
+# Use pointnet model to run a prediction on a numpy input, and return a string of the research group along with the output as a numpy array
+def get_prediction(model, input, device):
+
+    input = torch.from_numpy(input).type(torch.float32).to(device)
+
+    model = PointNetWrapper(model)
+
+    model.to(device)
+
+    mapping = {
+        0: 'CN',
+        1: 'MCI',
+    }
+
+    # Remove batch dim
+    output = model(input).squeeze(0)
+
+    pred_class = int(torch.argmax(torch.nn.functional.softmax(output, dim=0)).cpu().numpy())
+
+    pred_research_group = mapping.get(pred_class, -1)
+
+    output = output.detach().cpu().numpy()
+
+    return pred_research_group, pred_class, output
+
+def get_combined_prediction(subject_data):
+
+    # Load in the model 
+
+    # Run the prediction on the point cloud
+    model = pointnet2_cls_ssg.get_model(2, normal_channel=False)
+
+    model.load_state_dict(torch.load("/uolstore/home/student_lnxhome01/sc22olj/Compsci/year3/individual-project-COMP3931/individual-project-sc22olj/pipeline/current_model.pth", weights_only=True))
+
+    # Load in the subject's cloud
+    cloud = np.load(os.path.join(subject.path, 'Left-Hippocampus_cropped_mesh_downsampledcloud.npy'))
+
+    # Run the prediction
+    _, _, pointnet_output = get_prediction(model, cloud, 'cpu')
+
+    # Predict using the parcellation volumes
+    volumes_output = 
 
 
-model = pointnet2_cls_ssg.get_model(2, normal_channel=False)
+    # Predict using the scores
+    scores_output = 
 
-model.load_state_dict(torch.load("/uolstore/home/student_lnxhome01/sc22olj/Compsci/year3/individual-project-COMP3931/individual-project-sc22olj/current_model.pth", weights_only=True))
 
-get_prediction(model, cloud, 'cpu')
+    # Average the predictions
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -17,12 +17,84 @@ from preprocessing_post_fastsurfer.vis import *
 from preprocessing_post_fastsurfer.cropping import *
 from preprocessing_post_fastsurfer.mesh_creation import *
 from ..explain_pointnet import *
+from .combined_models import *
 from ozzy_torch_utils.split_dataset import *
 from ozzy_torch_utils.subject_dataset import *
 from ozzy_torch_utils.plot import *
 from ozzy_torch_utils.train_nn import *
 from ozzy_torch_utils.model_parameters import *
 from ozzy_torch_utils.init_dataloaders import *
+
+def process_single_subject(file_path, license_path, tesla3 = False):
+
+    # Make a copy in tmp for use in the pipeline
+    tmp_path = os.path.join("/tmp/mripredict/", filename)
+
+    shutil.copy(args.file_path, tmp_path)
+
+    # Load in nii image
+    process_file("/tmp/mripredict/", filename, args.license_path, 4, args.tesla3)
+
+    subject = Subject(tmp_path, None)
+
+    subject_data = {}
+
+    # Get reference brain
+    reference_brain_array = extract_brain("/uolstore/home/student_lnxhome01/sc22olj/Compsci/year3/individual-project-COMP3931/individual-project-sc22olj/mni_icbm152_lin_nifti/fastsurfer-processed/mri/orig_nu.mgz", "/uolstore/home/student_lnxhome01/sc22olj/Compsci/year3/individual-project-COMP3931/individual-project-sc22olj/mni_icbm152_lin_nifti/fastsurfer-processed/mri/mask.mgz")
+
+    # Align the MRI
+    alignment(subject, reference_brain_array)
+
+    # Align the aparc
+    aux_alignment(subject, subject.aparc, is_aparc=True)
+
+    # Extract the aligned left hippocampus
+    extract_region(subject, [17], subject.brain_aligned, subject.aparc_aligned, is_aligned=True)
+
+    # Get the bounding box of the hippocampus
+    hcampus_image_array = nibabel.load(os.path.join(subject.path, "Left-Hippocampus_aligned.nii")).get_fdata()
+
+    bbox = bounding_box(hcampus_image_array)
+
+    # Crop the left hippocampus
+    crop(subject, "Left-Hippocampus_aligned.nii", bbox)
+
+    # Convert the left hippocampus to mesh
+    volume_to_mesh(subject, 'Left-Hippocampus_aligned_cropped.nii', smooth=True, number_of_iterations=5, lambda_filter=1.2)
+
+    # Check there are enough points
+
+    # Downsample the cloud to 1048 points for pointnet
+    downsample_cloud(subject, "Left-Hippocampus_aligned_cropped_mesh.npz")
+
+    # Load in the cloud
+    subject_data['cloud'] = np.load(os.path.join(subject.path, 'Left-Hippocampus_cropped_mesh_downsampledcloud.npy'))
+
+    # Get the volumetric data - this code is lifted from ozzy_torch_utils
+    volume_col = subject.aseg_stats['Volume_mm3']
+
+    volume_col_normalised = volume_col / volume_col.sum() * 1000
+
+    struct_name_col = subject.aseg_stats['StructName']
+    
+    subject_data['volumes'] = np.array(volume_col_normalised)
+    
+    subject_data['struct_names'] = np.array(struct_name_col)
+
+    # Get the scores - NB this will be NULL unless we load in a csv!!
+    mmse = subject.subject_metadata['MMSE Total Score'].iloc[0]
+
+    gdscale = subject.subject_metadata['GDSCALE Total Score'].iloc[0]
+
+    faq = subject.subject_metadata['FAQ Total Score'].iloc[0]
+
+    npiq = subject.subject_metadata['NPI-Q Total Score'].iloc[0]
+
+    subject_data['scores'] = [mmse, gdscale, faq, npiq]
+
+    subject_data['score_names'] = ['MMSE Total Score', 'GDSCALE Total Score', 'FAQ Total Score', 'NPI-Q Total Score']
+
+    return subject_data
 
 # THIS WHOLE FILE NEEDS TO RUN IN A DOCKER CONTAINER
 if __name__ == "__main__":
@@ -86,5 +158,9 @@ if __name__ == "__main__":
 
     # Downsample the cloud to 1048 points for pointnet
     downsample_cloud(subject, "Left-Hippocampus_aligned_cropped_mesh.npz")
+
+    get_combined_prediction(subject)
+
+    # Need to be able to do explainability as well
 
     shutil.rmtree("/tmp/mripredict/")
